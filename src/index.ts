dba15b698416d3128558f8fa4beedd51be33f6ae
Mike Miller <mike.miller@binarydefense.com>
2020-06-27 14:01:36 -0400

    Creates main report generator
    This is some untested code I'm just going to yeet right into master

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,73 @@
 import { createFileReader, createConfig, hasValidRules } from './config';
 import { createOutdatedRequest, createDetailsRequest } from './npm-interactions';
 
-const main = config => {
-  // TODO validate rules of config
-  // TODO make npm outdated request
-  // TODO make view request for each outdated dependency
-  // TODO calculate delta between current version and latest
-  // TODO generate current, latest, days-expired report
+interface ReportData {
+  name: string,
+  current: string,
+  latest: string,
+  daysOutdated: number,
+  isOutdated: boolean,
+}
+
+interface OutdatedDependency {
+  current: string,
+  wanted: string,
+  latest: string,
+  location: string,
+}
+
+const Maybe = function (x) { this._value = x; };
+Maybe.of = (x) => new Maybe(x);
+Maybe.prototype.map = function (f) { return Maybe.of(f(this._value)); };
+
+// generateReport( config: object ) => Maybe()
+export const generateReport = async (config) => {
+  const isValid = hasValidRules(config.rules);
+
+  if (!isValid) return Maybe.of(new Error('Configuration contains invalid rules'));
+
+  const getOutdated = createOutdatedRequest();
+  let outdated: object;
+
+  try {
+    outdated = await getOutdated();
+  } catch (err) {
+    return Maybe.of(err);
+  }
+
+  try {
+    let reportData: ReportData[];
+
+    Object.entries(outdated).forEach(async (val) => {
+      const [name, desiredDetails]: [string, OutdatedDependency] = val;
+      const getDetails = createDetailsRequest(name);
+      const { time } = await getDetails();
+      const currentTime = time[desiredDetails.current];
+      const latestTime = time[desiredDetails.latest];
+      const currentDay = new Date(currentTime).getUTCDay();
+      const latestDay = new Date(latestTime).getUTCDay();
+      const daysOutdated = currentDay - latestDay;
+
+      let isOutdated: boolean;
+
+      if (!config.rules[name]) isOutdated = true;
+      else if (config.rules[name].ignore) isOutdated = false;
+      else if (config.rules[name].daysUntilExpiration < daysOutdated) isOutdated = true;
+      else isOutdated = false;
+
+      reportData.push({
+        name,
+        current: desiredDetails.current,
+        latest: desiredDetails.latest,
+        daysOutdated,
+        isOutdated,
+      });
+    });
+
+    return Maybe.of(reportData);
+  } catch (err) {
+    return Maybe.of(err);
+  }
 };
 
 export const configuration = {
@@ -23,4 +84,5 @@ export const npm = {
 export default {
   configuration,
   npm,
+  generateReport,
 };
