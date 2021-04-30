e5073e6f8cff8acc55764d9437b0159dd2e4c3a5
Mike Miller <ominestre@gmail.com>
2021-04-30 15:43:23 -0400

    Fixes issue with generating report before installing
    closes #3
    
    Adds a warning if you run `rotten-deps` before running `yarn|npm install`.
    This change also will use wanted version in place of current if you don't install first
    to avoid the NaN crash.
    
    - removes 'should' from several tests to save on text quota payments
    - updated rotten deps test runner to take `testcontainer` in the configuration to facilitate the no-install tests
    - fixed a critical typo in the wallaby conf: `wekk` -> `well`

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -8,7 +8,7 @@ module.exports = () => ({
   tests: [
     'test/*.test.js',
     'test/*.test.ts',
-    '!test/cli.test.ts', // Wallaby doesn't handle CLI testing wekk
+    '!test/cli.test.ts', // Wallaby doesn't handle CLI testing well
   ],
   env: {
     type: 'node',
